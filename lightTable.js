327f73d0d142a288b5d0c973c44dc9a1c4a267ad
j-beckman <beckman.joshua@gmail.com>
2014-01-28 23:10:37 -0600

    Adding IPTC support

--- lightTable.js
+++ lightTable.js
@@ -679,7 +679,103 @@
           StringValues : StringValues
       };
 
-    })()
+    })(),
+    getIPTC: function(img, callback) {
+      // This is very buggy right now, a bit unreliable as to what it can return
+      var bDebug = false;
+      var fieldMap = {
+        120 : 'caption',
+        110 : 'credit',
+        25 : 'keywords',
+        85 : 'byline',
+        122 : 'captionWriter',
+        105 : 'headline',
+        116 : 'copyright',
+        15 : 'category'
+      };
+      if (img instanceof Image && !img.complete) return false;
+      if (!imageHasData(img)) {
+          getImageData(img, callback);
+      } else {
+        if (callback) {
+          callback.call(img);
+        }
+      }
+      function imageHasData(img) {
+        return !!(img.exifdata);
+      }
+      function getImageData(img, callback) {
+        function handleBinaryFile(binFile) {
+          var data = findIPTCinJPEG(binFile);
+          iptcData = data || {};
+          if (callback) {
+            callback.call(iptcData);
+          }
+        }
+        if (img instanceof Image) {
+          BinaryAjax(img.src, function(http) {
+            handleBinaryFile(http.binaryResponse);
+          });
+        } else if (window.FileReader && img instanceof window.File) {
+          var fileReader = new FileReader();
+          fileReader.onload = function(e) {
+            handleBinaryFile(new BinaryFile(e.target.result));
+          };
+          fileReader.readAsBinaryString(img);
+        }
+      }
+      function readIPTCData(oFile, iStart, iLength) {
+        var data = {};
+        if (oFile.getStringAt(iStart, 9) != "Photoshop") {
+          if (bDebug) log("Not valid Photoshop data! " + oFile.getStringAt(iStart, 9));
+          return false;
+        }
+        var length, offset, fieldStart, title, value,
+          fileLength = oFile.getLength(),
+          FILE_SEPARATOR_CHAR = 28,
+          START_OF_TEXT_CHAR = 2;
+
+        for (var i = 0; i < iLength; i++) {
+          fieldStart = iStart + i;
+          if(oFile.getByteAt(fieldStart) == START_OF_TEXT_CHAR && oFile.getByteAt(fieldStart + 1) in fieldMap) {
+            length = 0;
+            offset = 2;
+            while(
+                fieldStart + offset < fileLength &&
+                oFile.getByteAt(fieldStart + offset) != FILE_SEPARATOR_CHAR &&
+                oFile.getByteAt(fieldStart + offset + 1) != START_OF_TEXT_CHAR) { offset++; length++; }
+            if(!length) { continue; }
+            title = fieldMap[oFile.getByteAt(fieldStart + 1)];
+            value = oFile.getStringAt(iStart + i + 2, length) || '';
+            value = value.replace('\000','').trim();
+            data[title] = value;
+            i+=length-1;
+          }
+        }
+        return data;
+      }
+      function findIPTCinJPEG(oFile) {
+        var aMarkers = [];
+        if (oFile.getByteAt(0) != 0xFF || oFile.getByteAt(1) != 0xD8) {
+          return false; // not a valid jpeg
+        }
+        var iOffset = 2;
+        var iLength = oFile.getLength();
+        while (iOffset < iLength) {
+          if (oFile.getByteAt(iOffset) != 0xFF) {
+            if (bDebug) console.log("Not a valid marker at offset " + iOffset + ", found: " + oFile.getByteAt(iOffset));
+            return false; // not a valid marker, something is wrong
+          }
+          var iMarker = oFile.getByteAt(iOffset+1);
+          if (iMarker == 237) {
+            if (bDebug) console.log("Found 0xFFED marker");
+            return readIPTCData(oFile, iOffset + 4, oFile.getShortAt(iOffset+2, true)-2);
+          } else {
+            iOffset += 2 + oFile.getShortAt(iOffset+2, true);
+          }
+        }
+      }
+    }
   };
   /**
    * Basic Javascript port of the MMCQ (modified median cut quantization)
